391a2e8c72ae627e915eb516285df46b73b4601a
Prajukta <prajuktaha@gmail.com>
2026-04-23 19:21:29 +0530

    Add middleware and update server listening port

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+require ('dotenv').config()
 const express = require ('express')
 //to express app
 const app = express()
+
+//middleware
+app.use((req , res , next) => {
+  console.log(req.path , req.method)
+  next()
+})
+//routes
+app.get('/' , (req , res) => {
+  res.json({mssg: "Welcome to app prajukta"})
+})
+app.get('/hello' , (req , res) =>{
+  res.json ({ mssg : "i hope u had a good day doing programming using"})
+})
 //listen for rquest
-app.listen(4000 , ()=>{
+app.listen(process.env.PORT , ()=>{
   console.log("Hi can u plz listen to port 4000")
 })
+
